13df867837c2f0ff5f4e1823ffd4710182ddbad7
MW116400 <MW116400@MGRW42NLPC2330>
2025-06-26 16:55:20 +0530

    all contreller and views

---
+++ ERPInventoryPurchesSystems/Controllers/CategoryController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ERPInventoryPurchesSystems.Models.Master;
+using ERPInventoryPurchesSystems.Utility;
+
+namespace ERPInventoryPurchesSystems.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> CategoryList()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateCategory(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(CategoryList));
+            }
+            return View(category);
+        }
+
+        public async Task<IActionResult> EditCategory(string id)
+        {
+            if (id == null) return NotFound();
+
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null) return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditCategory(string id, Category category)
+        {
+            if (id != category.CategoryCode) return BadRequest();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(category);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Categories.Any(e => e.CategoryCode == id))
+                        return NotFound();
+                    else
+                        throw;
+                }
+                return RedirectToAction(nameof(CategoryList));
+            }
+            return View(category);
+        }
+
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null) return NotFound();
+            return View(category);
+        }
+
+        [HttpPost, ActionName("DeleteCategory")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteCategoryConfirmed(string id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null) return NotFound();
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(CategoryList));
+        }
+
+        public async Task<IActionResult> DetailsCategory(string id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null) return NotFound();
+            return View(category);
+        }
+    }
+}
---
+++ ERPInventoryPurchesSystems/Controllers/VendorController.cs
@@ -0,0 +1,86 @@
+﻿using ERPInventoryPurchesSystems.Models.Master;
+using ERPInventoryPurchesSystems.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class VendorController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public VendorController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> VendorList()
+    {
+        return View(await _context.Vendors.ToListAsync());
+    }
+
+    public IActionResult CreateVendor()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> CreateVendor(Vendor vendor)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(vendor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(VendorList));
+        }
+        return View(vendor);
+    }
+
+    public async Task<IActionResult> EditVendor(string id)
+    {
+        if (id == null) return NotFound();
+
+        var vendor = await _context.Vendors.FindAsync(id);
+        if (vendor == null) return NotFound();
+
+        return View(vendor);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> EditVendor(string id, Vendor vendor)
+    {
+        if (id != vendor.VendorCode) return BadRequest();
+
+        if (ModelState.IsValid)
+        {
+            _context.Update(vendor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(VendorList));
+        }
+        return View(vendor);
+    }
+
+    public async Task<IActionResult> DeleteVendor(string id)
+    {
+        var vendor = await _context.Vendors.FindAsync(id);
+        if (vendor == null) return NotFound();
+        return View(vendor);
+    }
+
+    [HttpPost, ActionName("DeleteVendor")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteVendorConfirmed(string id)
+    {
+        var vendor = await _context.Vendors.FindAsync(id);
+        _context.Vendors.Remove(vendor);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(VendorList));
+    }
+
+    public async Task<IActionResult> DetailsVendor(string id)
+    {
+        var vendor = await _context.Vendors.FindAsync(id);
+        if (vendor == null) return NotFound();
+        return View(vendor);
+    }
+}
